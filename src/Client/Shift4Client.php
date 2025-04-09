dd5f3db1d1631a26b597caa75e536cd826c1970b
Wagdy <a-wagdy@outlook.com>
2025-04-09 22:02:20 +0200

    Created Shift4Client

---
+++ src/Client/Shift4Client.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Client;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class Shift4Client
+{
+    private const API_URL = 'https://api.shift4.com';
+    private const AUTH_KEY = 'sk_test_QhvkbCgZGSjMymoKuckXQfRq';
+
+    public function __construct(
+        private readonly HttpClientInterface $client,
+    ) {
+    }
+
+    public function createToken(array $cardData): array
+    {
+        $response = $this->client->request('POST', self::API_URL . '/tokens', [
+            'auth_basic' => [
+                self::AUTH_KEY,
+                ''
+            ],
+            'body' => $cardData,
+        ]);
+
+        return $response->toArray();
+    }
+
+    public function createCharge(array $chargeData): array
+    {
+        $response = $this->client->request('POST', self::API_URL . '/charges', [
+            'auth_basic' => [
+                self::AUTH_KEY,
+                ''
+            ],
+            'body' => $chargeData,
+        ]);
+
+        return $response->toArray();
+    }
+
+}
--- src/PaymentGateway/Shift4Gateway.php
+++ src/PaymentGateway/Shift4Gateway.php
@@ -5,63 +5,78 @@ declare(strict_types=1);
 namespace App\PaymentGateway;
 
 use DateTime;
+use App\Client\Shift4Client;
 use App\DTO\PaymentGatewayInputDto;
 use App\DTO\PaymentGatewayResponseDto;
 use App\Exception\PaymentProcessingException;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Shift4Gateway implements PaymentGatewayInterface
 {
-    private const API_URL = 'https://api.shift4.com';
-    private const AUTH_KEY = 'sk_test_QhvkbCgZGSjMymoKuckXQfRq';
-
     public function __construct(
-        private readonly HttpClientInterface $client,
+        private readonly Shift4Client $client,
     ) {
     }
 
+    /**
+     * @throws PaymentProcessingException
+     */
     public function processPayment(PaymentGatewayInputDto $dto): PaymentGatewayResponseDto
     {
-        $response = $this->client->request('POST', self::API_URL . '/tokens', [
-            'auth_basic' => [self::AUTH_KEY, ''],
-            'body' => [
-                'number' => '4242424242424242',
-                'expMonth' => $dto->cardExpMonth,
-                'expYear' => $dto->cardExpYear,
-                'cvc' => $dto->cardCvv,
-            ],
+        $token = $this->generateCardToken($dto);
+        $chargeData = $this->chargeCard($dto, $token);
+
+        $dateCreated = (new DateTime())->setTimestamp($chargeData['created']);
+
+        return new PaymentGatewayResponseDto(
+            currency: $chargeData['currency'],
+            transactionId: $chargeData['id'],
+            createdAt: $dateCreated->format('Y-m-d H:i:s'),
+            amount: $chargeData['amount'],
+            cardBin: $chargeData['card']['first6']
+        );
+    }
+
+    /**
+     * @param PaymentGatewayInputDto $dto
+     * @return string
+     * @throws PaymentProcessingException
+     */
+    private function generateCardToken(PaymentGatewayInputDto $dto): string
+    {
+        $tokenData = $this->client->createToken([
+            'number' => '4242424242424242',
+            'expMonth' => $dto->cardExpMonth,
+            'expYear' => $dto->cardExpYear,
+            'cvc' => $dto->cardCvv,
         ]);
 
-        $tokenData = $response->toArray();
         $token = $tokenData['id'] ?? null;
 
-        if (is_null($token)) {
+        if (!$token) {
             throw new PaymentProcessingException('Token creation failed');
         }
 
-        $response = $this->client->request('POST', self::API_URL . '/charges', [
-            'auth_basic' => [self::AUTH_KEY, ''],
-            'body' => [
-                'amount' => $dto->amount * 100,
-                'currency' => $dto->currency,
-                'card' => $token,
-            ],
-        ]);
+        return $token;
+    }
 
-        $chargeData = $response->toArray();
+    /**
+     * @param PaymentGatewayInputDto $dto
+     * @param string $token
+     * @return array
+     * @throws PaymentProcessingException
+     */
+    private function chargeCard(PaymentGatewayInputDto $dto, string $token): array
+    {
+        $chargeData = $this->client->createCharge([
+            'amount' => $dto->amount * 100,
+            'currency' => $dto->currency,
+            'card' => $token,
+        ]);
 
         if (isset($chargeData['error']) || $chargeData['status'] !== 'successful') {
             throw new PaymentProcessingException('Charge creation failed: ' . $chargeData['error']['message']);
         }
 
-        $date = (new DateTime())->setTimestamp($chargeData['created']);
-
-        return new PaymentGatewayResponseDto(
-            currency: $chargeData['currency'],
-            transactionId: $chargeData['id'],
-            createdAt: $date->format('Y-m-d H:i:s'),
-            amount: $chargeData['amount'],
-            cardBin: $chargeData['card']['first6']
-        );
+        return $chargeData;
     }
 }
